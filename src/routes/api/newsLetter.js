eeb295df1cd36f0ea741de8697cbcdfe0519da6c
yonas kemon <yonas@mohastech.com>
2023-03-10 08:45:57 +0300

    fixed issues regarding error with social login

--- src/routes/api/newsLetter.js
+++ src/routes/api/newsLetter.js
@@ -3,10 +3,9 @@ const newsLetterController = require("../../controllers/newsLetter");
 const router = express.Router();
 const auth = require("../../middlewares/auth");
 
-router.post(
-  "",
-  auth(),
-  newsLetterController.add
-);
+router.post("", auth(), newsLetterController.add);
 
+router.get("", auth(), newsLetterController.list);
+router.delete("/:id", auth(), newsLetterController.systemUnsubscribe);
+router.delete("", auth(), newsLetterController.unsubscribe);
 module.exports = router;
